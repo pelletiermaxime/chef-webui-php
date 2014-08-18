087adf280d22f08f22ec2e5471397a4c9e88afa8
Maxime Pelletier <pelletiermaxime@gmail.com>
2014-08-18 00:13:12 -0400

    Show the field values on error page. Somehow forgot the behat tests :S

--- app/views/databags/createItem.blade.php
+++ app/views/databags/createItem.blade.php
@@ -13,6 +13,21 @@
     <button type="button" class="btn btn-default add_field">Add field</button>
 </div>
 
+@if (isset(Input::old('item_name')))
+    @foreach (Input::old('item_name') as $fieldNo => $item_name)
+    <div class="form-group">
+        <label for="item_name">Item name</label>
+        <input id="item_name" class="form-control item_name" type="text"
+            name="item_name[]" value="{{ $item_name }}">
+        <label for="item_value">Item value</label>
+        <input id="item_value" class="form-control item_value" type="text"
+            name="item_value[]" value="{{ Input::old('item_value')[$fieldNo] }}">
+        <button type="button" class="btn btn-default add_field">Add field</button>
+        <button type="button" class="btn btn-default remove_field">Remove field</button>
+    </div>
+    @endforeach
+@endif
+
 <div class="form-group hidden add_field_template">
     <label for="item_name">Item name</label>
     <input id="item_name" class="form-control item_name" type="text">
