03d93b2cb3c177d2a0c8ba8617bf78586178dde7
Maxime Pelletier <pelletiermaxime@gmail.com>
2014-08-27 21:53:15 -0400

    Fix value printing

--- app/views/nodes/showItem.blade.php
+++ app/views/nodes/showItem.blade.php
@@ -1,9 +1,11 @@
 @foreach ($values as $index => $value)
     <li><?php echo($index)?>
-    @if (is_object($value))
+    @if (is_object($value) || is_array($value))
         <ul>
         @include('nodes.showItem', array('values' => $value))
         </ul>
+    @else
+        : {{ $value }}
     @endif
     </li>
 @endforeach
