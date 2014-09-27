6cc9269ec9cdfb4e013b74fa7c0b9887113cbc2a
Maxime Pelletier <pelletiermaxime@gmail.com>
2014-09-27 19:06:20 -0400

    Split js code in js file. Oh and spend yet again too many time on trying to make d&d tests work corretly :(

---
+++ public/assets/js/nodes.js
@@ -0,0 +1,41 @@
+function refreshRunList() {
+    var cookbooks = ''
+    $("#panel-available-run-list .run-list .available_recipe").each(function(index, el) {
+        cookbooks += ' ' + $.trim($(el).text());
+    });
+    $('#run_list').val(cookbooks);
+}
+
+$(function() {
+    $('.jstree').jstree({});
+    $(document).on('click', '.jstree-leaf', function() {
+        $(this).find('input').focus();
+    });
+    $(".available_recipe").draggable({
+        revert: "invalid",
+        helper: "clone",
+        cursor: "move",
+    });
+    $("#panel-available-run-list").droppable({
+        accept: ".available_recipes > li",
+        drop: function( event, ui ) {
+            ui.draggable.appendTo($(this).children('ul'));
+            refreshRunList();
+        },
+        tolerance: "touch"
+    });
+    $("#panel-available-recipes").droppable({
+        accept: ".run-list > li",
+        drop: function( event, ui ) {
+            ui.draggable.appendTo($(this).children('ul'));
+            setTimeout(refreshRunList, 100);
+        },
+        tolerance: "touch"
+    });
+
+    var options = {
+      valueNames: [ 'name' ]
+    };
+
+    var userList = new List('panel-available-recipes', options);
+});
