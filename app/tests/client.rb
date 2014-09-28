efe66294c5a8d04ae1acb493b3a14b8f689791b9
Maxime Pelletier <pelletiermaxime@gmail.com>
2014-09-28 00:22:22 -0400

    Well, relative path doesn't work, so expanding full path

--- app/tests/client.rb
+++ app/tests/client.rb
@@ -3,6 +3,6 @@ log_location             STDOUT
 node_name                'test-node-1'
 chef_server_url          'http://127.0.0.1:8889'
 
-client_key               'app/tests/key.pem'
+client_key               File.expand_path('app/tests/key.pem')
 syntax_check_cache_path  '~/.chef/syntax_check_cache'
 cookbook_path [ '~/.chef/chef-repo/cookbooks' ]
